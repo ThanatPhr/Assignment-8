8094b5a58584c7bfcd6366dd0557843e0b6872df
Thanat Phrudprisan <thanat.phr@gmail.com>
2023-03-22 19:47:30 +0700

    Assignment-7

---
+++ routes/hospitals.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const {
+  getHospitals,
+  createHospital,
+  getHospital,
+  deleteHospital,
+  updateHospital,
+} = require("../controller/hospitals");
+
+const appointmentRouter = require("./appointments");
+
+const router = express.Router();
+
+const { protect, authorize } = require("../middleware/auth");
+
+router.use("/:hospitalId/appointments", appointmentRouter);
+
+router
+  .route("/")
+  .get(getHospitals)
+  .post(protect, authorize("admin"), createHospital);
+router
+  .route("/:id")
+  .get(getHospital)
+  .delete(protect, authorize("admin"), deleteHospital)
+  .put(protect, authorize("admin"), updateHospital);
+
+module.exports = router;
